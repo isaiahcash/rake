059e03dfba2dea8fcc6ede0f2a1658a9ae04f76e
Isaiah Cash <isaiahcash@gmail.com>
2021-05-21 19:58:59 +0000

    Adding source files for DataTables.

--- includes/functions.php
+++ includes/functions.php
@@ -28,8 +28,8 @@ function script_includes()
     <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <script src="https://isaiahcash.com/business/includes/source/datatables/datatables.js"></script>
-    <script src="https://isaiahcash.com/business/includes/source/datatables/DataTables-1.10.18/js/dataTables.bootstrap4.js"></script>
+    <script src="https://isaiahcash.com/rake/includes/source/datatables/datatables.js"></script>
+    <script src="https://isaiahcash.com/rake/includes/source/datatables/DataTables-1.10.18/js/dataTables.bootstrap4.js"></script>
     <script src="https://isaiahcash.com/rake/includes/js/scripts.js"></script>
     <?php
 }
