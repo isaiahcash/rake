32270c41a05fc6fac041f2aafed0c52887096ab1
Isaiah Cash <isaiahcash@gmail.com>
2023-03-08 00:15:55 +0000

    Updating navigation

--- includes/config.php
+++ includes/config.php
@@ -9,4 +9,4 @@ require_once(__DIR__ . '/functions.php');
 require_once(__DIR__ . '/mysql.php');
 require_once(__DIR__ . '/../simplehtmldom/simple_html_dom.php');
 
-require_once(__DIR__ . '/navigate_home.php');
+require_once(__DIR__ . '/../../shared/navigate_home.php');
